4ac387c1288820ec0c450ff6be9e6b0431a261b7
Veniamin <112535019+Veniamin-Noise@users.noreply.github.com>
2023-01-03 15:08:23 +0600

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 function toReadable(number) {
     let result = ""
 
-    numbers = [['one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'], ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']]
+    numbers = [['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'], ['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']]
 
     if (99 < number && number < 1000) {
-        result = result + numbers[0][number.toString().slice(-3, -2) - 1] + ' hundred '
+        result = result + numbers[0][number.toString().slice(-3, -2)] + ' hundred '
     }
     if (19 < number.toString().slice(-2) && number.toString().slice(-2) < 100) {
         result = result + numbers[1][number.toString().slice(-2, -1) - 2] + ' '
-        result = result + numbers[0][number.toString().slice(-1) - 1]
+        result = result + numbers[0][number.toString().slice(-1)]
     }
-    if (number.toString().slice(-2) < 19) result = result + numbers[0][number.toString().slice(-2) - 1]
-    
+    if (number.toString().slice(-2) < 20) result = result + numbers[0][parseInt(number.toString().slice(-2))]
+
     return result
 }
